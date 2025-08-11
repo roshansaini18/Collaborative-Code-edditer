d7552ff58981d9c31ab9ca6d11091f6ddc15d680
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2025-08-12 04:07:57 +0530

    Update index.jsx

--- frontend/components/OutputAndInputPanel/index.jsx
+++ frontend/components/OutputAndInputPanel/index.jsx
@@ -1,24 +1,17 @@
-import React from 'react';
 
-/**
- * Renders the input and output consoles as resizable panels.
- */
-const OutputAndInputPanel = ({ input, setInput, output }) => (
+
+
+const OutputAndInputPanel = ({ input, setInput }) => (
   <div className="flex-grow flex flex-col relative">
     {/* Input Console */}
-    <div className="bg-gray-900 text-white p-4 font-mono overflow-auto shadow-inner" style={{ height: '50%' }}>
+    <div className="bg-gray-900 text-white p-4 font-mono overflow-auto shadow-inner" style={{ height: '100%' }}>
       <h3 className="font-bold mb-2">Input</h3>
       <textarea
-        className="w-full h-full p-2 bg-gray-800 text-white font-mono rounded-md resize-none focus:outline-none"
+        className="w-full h-full p-2 bg-gray-900 text-white font-mono rounded-md resize-none focus:outline-none"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Enter input here..."
       />
-    </div>
-
-    {/* Output Console */}
-    <div className="bg-gray-900 text-white p-4 font-mono overflow-auto rounded-b-lg shadow-inner" style={{ height: '50%' }}>
-      <pre>{output}</pre>
     </div>
   </div>
 );

dd270656dd104a10ee32fab4d36479e100a8f143
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2025-08-16 03:41:20 +0530

    Update index.jsx

--- frontend/components/OutputAndInputPanel/index.jsx
+++ frontend/components/OutputAndInputPanel/index.jsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-
-// /**
-//  * Renders the input and output consoles as resizable panels.
-//  */
-// const OutputAndInputPanel = ({ input, setInput}) => (
-//   <div className="flex-grow flex flex-col relative">
-//     {/* Input Console */}
-//     <div className="bg-gray-900 text-white p-4 font-mono overflow-auto shadow-inner" style={{ height: '50%' }}>
-//       <h3 className="font-bold mb-2">Input</h3>
-//       <textarea
-//         className="w-full h-full p-2 bg-gray-800 text-white font-mono rounded-md resize-none focus:outline-none"
-//         value={input}
-//         onChange={(e) => setInput(e.target.value)}
-//         placeholder="Enter input here..."
-//          style={{height:'700px'}}
-//       />
-//     </div>
-//   </div>
-// );
-
-// export default OutputAndInputPanel;
-
-
-
 // frontend/src/components/OutputAndInputPanel.js
 import React from 'react';
 
